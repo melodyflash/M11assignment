bfa022c392d400112ab8d11cb7db8b749854b511
Melody <melodyflash@gmail.com>
2024-01-07 16:33:17 -0800

    Math almost done

--- js/math.js
+++ js/math.js
@@ -1,9 +1,32 @@
 //STEP 1
+// let userNumber = prompt('Enter a number')
+
+// alert(`${Math.abs(userNumber)}`)
 
 //STEP 2
+// let userNumber = parseFloat(prompt('Enter a decimal number'))
+
+// alert(`Your number rounded t the nearest whole number is: ${Math.round(userNumber)}`)
 
 //STEP 3
+// let userNumber = parseFloat(prompt('Enter a decimal number'))
+
+// alert(`Your number rounded t the nearest whole number is: ${Math.floor(userNumber)}`)
+
 
 //STEP 4
 
+// let numList = prompt('Enter 5 numbers separated by commas. Example: 1,5,7,8,9')
+
+
+
+
+// console.log(numList)
+
+// alert(`The smallest and largest numbers are: ${Math.min(newArray)} , ${Math.max(newArray)}`)
+
+
 //STEP 5
+
+// let userNumber = parseInt(prompt('Enter a number to find the aquare root of'))
+// alert(`The square root of ${userNumber} is ${Math.sqrt(userNumber)}`)
