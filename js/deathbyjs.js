e9e45a334dd13c0c7f697ef67844e0cd113b318a
Melody <melodyflash@gmail.com>
2024-01-08 18:15:39 -0800

    deathbyjs partially done - ran out of steam and time

--- js/deathbyjs.js
+++ js/deathbyjs.js
@@ -1,9 +1,34 @@
 //STEP 1
 
+// function alphOrder(string) {
+//     return string.split("")
+//                 .sort()
+//                 .join("")
+// }
+
+// string=prompt("Enter string or word to organize alphabetically")
+
+// console.log(alphOrder(string))
+
 //STEP 2
+// function toCap(string) {
+//     words = string.split(" ")
+//     for (let i = 0; i < words.length; i++) {
+//         words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+//     }
+    
+//     return words.join(" ");
+// }
+// string=prompt("Enter string or words to capitalize")
+
+// console.log(toCap(string))
 
 //STEP 3
 
+
+
 //STEP 4
+
+
 
 //STEP 5
